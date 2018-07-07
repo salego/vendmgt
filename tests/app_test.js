2061b76e79cb3de15d69d24abeaf34ea798ffbf1
salego <40881404+salego@users.noreply.github.com>
2018-07-07 07:30:31 +0530

    Update app_test.js

--- tests/app_test.js
+++ tests/app_test.js
@@ -1,9 +1,7 @@
 var server   = require('../index'),
-    chai     = require('chai'),
-    chaiHTTP = require('chai-http'),
-    should   = chai.should();
 
-chai.use(chaiHTTP);
+
+
 
 //reqServer = process.env.HTTP_TEST_SERVER || server
 
